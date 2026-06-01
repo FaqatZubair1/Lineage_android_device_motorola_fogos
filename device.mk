2624446133d2ce3c47ad57cf8f2481f35d251c70
Michael Bestas <mkbestas@lineageos.org>
2026-06-01 17:21:33 +0300

    normal bringup

--- device.mk
+++ device.mk
@@ -28,6 +28,7 @@ PRODUCT_PACKAGES += \
     FrameworksResDeviceXT2363-9 \
     LineageApertureAppDevice \
     ProductFrameworksResDevice \
+    SettingsResDevice \
     SystemUIResDevice \
     TurboPowerOverlay \
     WifiResDevice \
