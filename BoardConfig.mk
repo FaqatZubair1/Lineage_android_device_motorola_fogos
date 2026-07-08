676e25df698b4770cdf9c779ad8b50b9c3e48066
FaqatZubair1 <161596117+FaqatZubair1@users.noreply.github.com>
2026-07-08 15:03:05 +0600

    Update BoardConfig.mk
    Gives error, needs to be added to device.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,4 @@
-#
+u#
 # SPDX-FileCopyrightText: The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -53,5 +53,3 @@ BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX := 30
 
 # inherit from the proprietary version
 include vendor/motorola/fogos/BoardConfigVendor.mk
-# Bypass kernel VINTF check for 5.4 kernel on Android 16
-PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
